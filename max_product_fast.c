b3e70d1637388b5db0c32727952babee1c180a6e
Steven Li <zeli17@simons-rock.edu>
2019-01-30 16:45:48 -0500

    'finished'

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,20 @@
 #include "max_product.h"
-
+#define SWAP(a,b) {int temp; temp=a; a=b; b=temp;}
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
-}
+        int index = 0;
+        for (int i = 1; i < size; i ++){
+                if (arr[index] < arr[i]){
+                        index = i;
+                }
+        }
+        SWAP(arr[index], arr[size - 1]);
+        index = 0;
+        for (int j = 1; j < size - 1; j ++){
+                if (arr[index] < arr[j]){
+                        index = j;
+                }
+        }
+        SWAP(arr[index], arr[size - 2]);
 
+        return arr[size - 1] * arr[size - 2];
+}
--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,12 @@
 #include "max_product.h"
+#define max(a,b) (a>b?a:b)
 
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+        int product = 0;
+        for (int i = 0; i < size; i ++){
+                for (int j = i + 1; j < size; j ++){
+                        product = max(product, arr[i] * arr[j]);
+                }
+        }
+        return product;
 }
